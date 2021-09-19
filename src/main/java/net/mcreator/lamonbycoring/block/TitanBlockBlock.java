faa59262c69e044a89c82fed8f5443762992bd1a
CoringPlay <vvvfiar@gmail.com>
2021-09-19 21:13:30 +1000

    Fix License

---
+++ src/main/java/net/mcreator/lamonbycoring/block/TitanBlockBlock.java
@@ -0,0 +1,56 @@
+
+package net.mcreator.lamonbycoring.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.lamonbycoring.LamonByCoringModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@LamonByCoringModElements.ModElement.Tag
+public class TitanBlockBlock extends LamonByCoringModElements.ModElement {
+	@ObjectHolder("lamon_by_coring:titan_block")
+	public static final Block block = null;
+	public TitanBlockBlock(LamonByCoringModElements instance) {
+		super(instance, 36);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(2f, 10f).setLightLevel(s -> 0));
+			setRegistryName("titan_block");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
