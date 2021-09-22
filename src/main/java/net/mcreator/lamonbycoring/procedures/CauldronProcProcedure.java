b771cbb7223ecd2c82cea8cdac60cbe00a64c968
CoringPlay <vvvfiar@gmail.com>
2021-09-22 23:40:36 +1000

    More Precedur

---
+++ src/main/java/net/mcreator/lamonbycoring/procedures/CauldronProcProcedure.java
@@ -0,0 +1,8 @@
+package net.mcreator.lamonbycoring.procedures;
+
+import java.util.Map;
+
+public class CauldronProcProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+	}
+}
