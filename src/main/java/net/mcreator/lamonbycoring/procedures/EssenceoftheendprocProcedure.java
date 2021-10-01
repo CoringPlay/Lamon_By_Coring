3c644c84f54fb1b50f000f4117811d4544593b06
CoringPlay <vvvfiar@gmail.com>
2021-10-01 14:50:22 +1000

    Add essence of the end

---
+++ src/main/java/net/mcreator/lamonbycoring/procedures/EssenceoftheendprocProcedure.java
@@ -0,0 +1,85 @@
+package net.mcreator.lamonbycoring.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.monster.EndermanEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.lamonbycoring.item.EssenceoftheEndItem;
+import net.mcreator.lamonbycoring.LamonByCoringMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class EssenceoftheendprocProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onEntityDeath(LivingDeathEvent event) {
+			if (event != null && event.getEntity() != null) {
+				Entity entity = event.getEntity();
+				Entity sourceentity = event.getSource().getTrueSource();
+				double i = entity.getPosX();
+				double j = entity.getPosY();
+				double k = entity.getPosZ();
+				World world = entity.world;
+				Map<String, Object> dependencies = new HashMap<>();
+				dependencies.put("x", i);
+				dependencies.put("y", j);
+				dependencies.put("z", k);
+				dependencies.put("world", world);
+				dependencies.put("entity", entity);
+				dependencies.put("sourceentity", sourceentity);
+				dependencies.put("event", event);
+				executeProcedure(dependencies);
+			}
+		}
+	}
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				LamonByCoringMod.LOGGER.warn("Failed to load dependency entity for procedure Essenceoftheendproc!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				LamonByCoringMod.LOGGER.warn("Failed to load dependency x for procedure Essenceoftheendproc!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				LamonByCoringMod.LOGGER.warn("Failed to load dependency y for procedure Essenceoftheendproc!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				LamonByCoringMod.LOGGER.warn("Failed to load dependency z for procedure Essenceoftheendproc!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				LamonByCoringMod.LOGGER.warn("Failed to load dependency world for procedure Essenceoftheendproc!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((entity instanceof EndermanEntity)) {
+			if ((Math.random() < 0.3)) {
+				if (world instanceof World && !world.isRemote()) {
+					ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(EssenceoftheEndItem.block));
+					entityToSpawn.setPickupDelay((int) 10);
+					world.addEntity(entityToSpawn);
+				}
+			}
+		}
+	}
+}
